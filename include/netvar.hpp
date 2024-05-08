9f02fd4770a97e0fa8cb90857197f41871d55b89
astolfo <astolfo.gman@gmail.com>
2024-05-08 16:46:48 -0600

    modified netvar.hpp
    starting to flesh out auto-synchronization design.

--- include/netvar.hpp
+++ include/netvar.hpp
@@ -1,26 +1,32 @@
 #ifndef NETVAR_HPP
 #define NETVAR_HPP
 
+#include <cstdlib>
 #include <cstring>
-#include <stdexcept>
 #include <string>
+#include <unordered_map>
 #include <uuid/uuid.h>
+#include <vector>
 
 #include "rpc_node.hpp"
 #include "tcp.hpp"
 
-template <typename T> struct netvar : T {
-  erpc_node<tcp_socket> *netvar_transport = nullptr;
+template <typename T> struct netvar {
+  static erpc_node<tcp_socket> netvar_transport;
+  static std::unordered_map<std::string, netvar<T> *> netvar_lookup;
 
   // implicit getter.
-  T &operator=(const netvar<T> &net_obj) { return net_obj.obj; }
+  // T &operator=(const netvar<T> &net_obj) { return net_obj.obj; }
 
   // implicit setter.
-  netvar<T> &operator=(const T &obj) {
+  // netvar<T> &operator=(const T &obj) {
+  void set(const T &obj) {
 
     // set for self.
-    this->obj = obj;
-
+    if (is_local) {
+      this->obj = obj;
+      modified = true;
+    }
     // The following MAY cause a broadcast storm. make sure to verify this.
     // EDIT: just logically i can see that this would definitely cause a
     // broadcast storm, so ill have to implement a "broadcast" function in the
@@ -30,34 +36,78 @@ template <typename T> struct netvar : T {
     // software-routable ID's to increase effectiveness of synchronization,
     // reduce chances of broadcast looping, capability of multicasting.
 
-    // set for remotes.
-    for (auto &provider : netvar_transport->providers)
-      netvar_transport->call(
-          provider, [this](T obj) { this->obj = obj; }, obj);
+    // set for remotes upstream.
+    for (auto &provider : netvar_transport.providers)
+      netvar_transport.call(
+          provider,
+          [this](T obj, std::string uuid) {
+            auto iter = netvar_lookup.find(uuid);
+            if (iter != std::end(netvar_lookup) && !is_local)
+              this->obj = obj;
+          },
+          obj);
 
-    // set for remotes.
-    for (auto &subscriber : netvar_transport->subscribers)
-      netvar_transport->call(
-          subscriber, [this](T obj) { this->obj = obj; }, obj);
+    // set for remotes downstream.
+    for (auto &subscriber : netvar_transport.subscribers)
+      netvar_transport.call(
+          subscriber,
+          [this](T obj, std::string uuid) {
+            auto iter = netvar_lookup.find(uuid);
+            if (iter != std::end(netvar_lookup) && !is_local)
+              this->obj = obj;
+          },
+          obj);
   }
 
   netvar(T &object) {
-    if (netvar_transport == nullptr)
-      throw std::runtime_error("netvar_transport has not been initialized");
+    is_local = true;
+    modified = false;
+    this->object = std::move(object);
 
-    id = netvar_transport->call(
-        netvar_transport->providers[0], []() -> std::string {
+    // TODO: authorities need to be determined. i believe it should be the
+    // users' authority for its own local stuff. figuring out enforcing
+    // different restrictions are to be determined at a higher level. Ex. player
+    // shouldnt directly control their position. but should absolutely control
+    // their keyboard/mouse inputs.
+    id = netvar_transport.call(
+        netvar_transport.providers[0],
+        [](T obj) -> std::string {
           char *id;
           uuid_generate_random(reinterpret_cast<unsigned char *>(id));
           std::string strid(id, strlen(id));
-          return strid;
-        });
 
-    *this = object;
-    modified = false;
+          netvar<T> *nv = (netvar<T> *)malloc(sizeof(netvar<T>));
+          nv->object = std::move(obj);
+          nv->is_local = false;
+          nv->modified = false;
+          nv->id = strid;
+
+          netvar<T>::netvar_lookup.emplace(strid, std::move(nv));
+          return strid;
+        },
+        this->object);
+
+    netvar<T>::netvar_lookup.emplace(id, this);
+  }
+
+  // TODO: implement ownership functionality. and a way to verify the
+  // authenticity of the request.
+
+  ~netvar() {
+    netvar_transport.call(
+        netvar_transport.providers[0],
+        [this](std::string id) {
+          auto iter = netvar_lookup.find(id);
+          if (iter != std::end(netvar_lookup)) {
+            free(*iter);
+            std::erase(iter);
+          }
+        },
+        id);
   }
 
 private:
+  T object;
   std::string id;
   bool modified;
   bool is_local;
