40cdc5076cd2c2485b656689234be5dc5b23261f
astolfo <astolfo.gman@gmail.com>
2024-05-07 21:05:42 -0600

    modified netvar.hpp

--- include/netvar.hpp
+++ include/netvar.hpp
@@ -9,16 +9,36 @@
 #include "rpc_node.hpp"
 #include "tcp.hpp"
 
-template <typename T> struct netvar {
-  static erpc_node<tcp_socket> *netvar_transport;
+template <typename T> struct netvar : T {
+  erpc_node<tcp_socket> *netvar_transport = nullptr;
 
   // implicit getter.
   T &operator=(const netvar<T> &net_obj) { return net_obj.obj; }
 
   // implicit setter.
   netvar<T> &operator=(const T &obj) {
+
+    // set for self.
     this->obj = obj;
-    modified = true;
+
+    // The following MAY cause a broadcast storm. make sure to verify this.
+    // EDIT: just logically i can see that this would definitely cause a
+    // broadcast storm, so ill have to implement a "broadcast" function in the
+    // erpc_node, which allows users to broadcast to the rest of the network in
+    // a way where it does not also return to sender.
+    // NOTE: would this also be reasoning to introduce meshing? (making
+    // software-routable ID's to increase effectiveness of synchronization,
+    // reduce chances of broadcast looping, capability of multicasting.
+
+    // set for remotes.
+    for (auto &provider : netvar_transport->providers)
+      netvar_transport->call(
+          provider, [this](T obj) { this->obj = obj; }, obj);
+
+    // set for remotes.
+    for (auto &subscriber : netvar_transport->subscribers)
+      netvar_transport->call(
+          subscriber, [this](T obj) { this->obj = obj; }, obj);
   }
 
   netvar(T &object) {
@@ -33,12 +53,11 @@ template <typename T> struct netvar {
           return strid;
         });
 
-    obj = object;
+    *this = object;
     modified = false;
   }
 
 private:
-  T obj;
   std::string id;
   bool modified;
   bool is_local;
